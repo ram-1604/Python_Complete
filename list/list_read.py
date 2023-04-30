3d039bd2ed78f0b56d240841d67dbfc1d0260869
Ram <45118457+ram-1604@users.noreply.github.com>
2023-04-30 21:15:55 +0530

    Create list_read.py

---
+++ list/list_read.py
@@ -0,0 +1,36 @@
+# How to read elements/items from list?
+# 1. Index
+# 2. Slicing
+# 3. Iterator
+# 4. Enumerate
+# 5. For loop
+
+
+# Index
+# What is index?
+# Index is an integer value which is used to identify each location or value in
+# list/sequence. This index can be +ve or –ve
+# +VE index starts at 0, which is used to read elements in forward direction
+# (Left to Right)
+# -VE index starts at -1, which is used to read elements in backward direction
+# (Right to Left)
+# Index is used to read only one value/element from list
+# It allows reading in sequential or random order
+
+list1=[10,20,30,40,50]
+print(list1[0])
+print(list1[1])
+print(list1[2])
+print(list1[3])
+print(list1[4])
+print("*********************")
+print(list1[-1])
+print(list1[-2])
+print(list1[-3])
+print(list1[-4])
+print(list1[-5])
+
+
+
+
+
